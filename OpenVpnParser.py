7788731e6d78dea7bea903d4d8703d728e9d0239
Flavio Pace <flavio.pace@gmail.com>
2019-01-25 20:45:21 +0100

    fix call

--- OpenVpnParser.py
+++ OpenVpnParser.py
@@ -56,7 +56,7 @@ class RunCommand(object):
 
         print ipcmd
 
-        p = Popen(ipcmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        p = subprocess.Popen(ipcmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
         output, err = p.communicate(b"input data that is passed to subprocess' stdin")
         rc = p.returncode
 
